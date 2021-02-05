f22a982dcbfab0fbf9e3b62e84e64a8b3ed68027
Aaron Paul Henry <aaron51598@gmail.com>
2021-02-04 22:33:36 -0500

    Update PitchLocatorPython.py

--- PitchLocatorPython.py
+++ PitchLocatorPython.py
@@ -16,7 +16,6 @@ from sqlalchemy import create_engine # database connection
 import datetime as dt
 from IPython.display import display
 
-# import chart_studio.plotly as py # interactive graphing
 import plotly.io as pio
 import plotly.graph_objs as go
 import plotly.offline as py
@@ -32,7 +31,6 @@ from baseball_scraper import statcast_pitcher
 
 pd.read_csv('JacobDegrom.csv', nrows=2).head()
 
-#wc -l < JacobDegrom.csv # Number of lines in dataset IN TERMINAL
 
 disk_engine = create_engine('sqlite:///JacobDegrom.db') # Initializes database with filename 311_8M.db in current directory
 
@@ -44,25 +42,10 @@ index_start = 1
 
 for df in pd.read_csv('JacobDegrom.csv', chunksize=chunksize, iterator=True, encoding='utf-8'):
 
-    df = df.rename(columns={c: c.replace(' ', '') for c in df.columns}) # Remove spaces from columns
-
-   # df['CreatedDate'] = pd.to_datetime(df['CreatedDate']) # Convert to datetimes
-   # df['ClosedDate'] = pd.to_datetime(df['ClosedDate'])
-
+    #cleaning column titles
+    df = df.rename(columns={c: c.replace(' ', '') for c in df.columns}) 
     df.index += index_start
-
-   # # Remove the un-interesting columns
-   # columns = ['Agency', 'CreatedDate', 'ClosedDate', 'ComplaintType', 'Descriptor',
-   #            'CreatedDate', 'ClosedDate', 'TimeToCompletion',
-   #           'City']
-
-   # for c in df.columns:
-   #    if c not in columns:
-   #        df = df.drop(c, axis=1)
-
-
     j+=1
-    #print '{} seconds: completed {} rows'.format((dt.datetime.now() - start).seconds(), j*chunksize)
 
     df.to_sql('data', disk_engine, if_exists='append')
     index_start = df.index[-1] + 1
