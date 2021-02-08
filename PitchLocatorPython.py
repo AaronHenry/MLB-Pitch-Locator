e62f1ce9802e2d80c4a468e4758bfada8e2fffa0
Aaron Paul Henry <aaron51598@gmail.com>
2021-02-08 17:08:06 -0500

    Add files via upload

--- PitchLocatorPython.py
+++ PitchLocatorPython.py
@@ -7,20 +7,13 @@ Created on Mon Feb  1 14:30:41 2021
 """
 
 
-import plotly
-import plotly.tools as tls
-
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine # database connection
-import datetime as dt
-from IPython.display import display
 
+# import chart_studio.plotly as py # interactive graphing
 import plotly.io as pio
 import plotly.graph_objs as go
-import plotly.offline as py
-import plotly.express as px
-from plotly.subplots import make_subplots
 pio.renderers.default='browser'
 
 from baseball_scraper import playerid_lookup
@@ -29,21 +22,24 @@ from baseball_scraper import statcast_pitcher
 
 #READ IN LOCAL CSV to SQLITE DB
 
-pd.read_csv('JacobDegrom.csv', nrows=2).head()
+# Initializes database with filename in current working directory
+disk_engine = create_engine('sqlite:///JacobDegrom.db') 
 
+# Read in CSV in chunks
 
-disk_engine = create_engine('sqlite:///JacobDegrom.db') # Initializes database with filename 311_8M.db in current directory
-
-start = dt.datetime.now()
-
+#Setting desired chunksize
 chunksize = 100
+
+#Indexing tools
 j = 0
 index_start = 1
 
 for df in pd.read_csv('JacobDegrom.csv', chunksize=chunksize, iterator=True, encoding='utf-8'):
-
-    #cleaning column titles
+    
+    # Remove spaces from columns
     df = df.rename(columns={c: c.replace(' ', '') for c in df.columns}) 
+
+
     df.index += index_start
     j+=1
 
@@ -52,7 +48,6 @@ for df in pd.read_csv('JacobDegrom.csv', chunksize=chunksize, iterator=True, enc
     
     
 #------------------------------------------------------------
-    
     
 #pitch locations SQL query
 heat = pd.read_sql_query('SELECT plate_x, plate_z, pitch_name, stand '
@@ -63,13 +58,16 @@ fig3 = go.Figure()
 for p in heat['pitch_name'].unique()[:4]:
     for s in heat['stand'].unique()[:2]:
         df = heat[heat['pitch_name']==p]
-    
-        fig3.add_traces(go.Scatter(x=df.loc[df['stand'] == s, 'plate_x'], y=df.loc[df['stand'] == s, 'plate_z'], name = p+" "+s+" "+"Handed", mode="markers"))
-    
-
+        fig3.add_traces(go.Scatter(x=df.loc[df['stand'] == s, 'plate_x'], 
+                                   y=df.loc[df['stand'] == s, 'plate_z'], 
+                                   name = p+" "+s+" "+"Handed", mode="markers"))
+#Strikezone
 fig3.add_trace(
-    go.Scatter(x=[-0.85,0.85,0.85,-0.85,-0.85], y=[3.5,3.5,1.5,1.5,3.5], mode="lines", showlegend=False))
+    go.Scatter(x=[-0.85,0.85,0.85,-0.85,-0.85], 
+               y=[3.5,3.5,1.5,1.5,3.5], 
+               mode="lines", showlegend=False))
 
+#Axes and titling / saving
 fig3.layout.xaxis.range = [-2, 2]
 fig3.layout.yaxis.range = [0, 5]
 fig3.update_layout(height=700, width=700, title_text="L/R Jacob Degrom 2020 Pitch Location Plots")
@@ -77,6 +75,7 @@ fig3.write_html("LR JacobDegrom2020.html")
 
 fig3.show()
 
+#------------------------------------------------------------
 
 #using statcast directly --- YU DARVISH 2020 EXAMPLE
 
@@ -97,14 +96,14 @@ for p in darvish_stats['pitch_name'].unique()[:6]:
             name = p+" "+s+" "+"Handed", mode="markers"))
     
 fig4.add_trace(
-    go.Scatter(x=[-0.85,0.85,0.85,-0.85,-0.85], y=[3.5,3.5,1.5,1.5,3.5], mode="lines", showlegend=False))
+    go.Scatter(x=[-0.85,0.85,0.85,-0.85,-0.85], 
+               y=[3.5,3.5,1.5,1.5,3.5], 
+               mode="lines", showlegend=False))
 
 fig4.update_traces(marker=dict(opacity=0.8))
 fig4.layout.xaxis.range = [-2, 2]
 fig4.layout.yaxis.range = [0, 5]
 fig4.update_layout(height=700, width=700, title_text="L/R Yu Darvish 2020 Pitch Locator")
-
-
 fig4.write_html("LR YuDarvish2020.html")
 
 fig4.show()
